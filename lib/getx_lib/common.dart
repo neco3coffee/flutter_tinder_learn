beca5016e93ad7e35609f27c3167a3c243b48416
neco3coffee <neco3coffee@gmail.com>
2021-10-10 01:12:34 +0900

    Add tinder-UI

--- lib/getx_lib/common.dart
+++
@@ -1,6 +0,0 @@
-// devtool
-import 'dart:developer' as dev;
-
-devlog(String description) {
-  dev.log("${description}\n${StackTrace.current.toString().split("#")[2]}");
-}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+// import 'package:tinder_clone/pages/root_app.dart';
+import 'package:flutter_tinder_learn/pages/root_app.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootPage(),
+  ));
+}
