2f98dcf82f70661cd7addcc4ca28b00b78d95ce7
neco3coffee <neco3coffee@gmail.com>
2021-10-14 08:58:55 +0900

    [SETUP] firebase android platform setup

--- lib/app/modules/stack/views/stack_view.dart
+++ lib/app/modules/stack/views/stack_view.dart
@@ -18,6 +18,55 @@ class StackView extends GetView<StackController> {
           style: TextStyle(fontSize: 20),
         ),
       ),
+      bottomSheet: getBottomSheet(),
+    );
+  }
+
+  Widget getBottomSheet() {
+    return Container(
+      width: 500,
+      height: 150,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {
+                Get.toNamed('/note');
+              },
+              icon: Icon(
+                Icons.note_alt_outlined,
+                color: Colors.pink,
+                size: 42,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Get.toNamed('/home');
+              },
+              icon: Icon(
+                Icons.play_circle_filled,
+                color: Colors.pink,
+                size: 42,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Get.toNamed('/stack');
+              },
+              icon: Icon(
+                Icons.file_copy_outlined,
+                color: Colors.pink,
+                size: 42,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
