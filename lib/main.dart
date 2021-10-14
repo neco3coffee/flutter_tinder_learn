342e8671e0907869ab66f161e5f039484dadbcb3
neco3coffee <neco3coffee@gmail.com>
2021-10-14 13:40:27 +0900

    Add WidgetsFlutterBinding.ensureInitialized() to main() #18

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(home: App()));
 }
 
