e97abdbe7dbf2b8624af93ac004751a9ebf14fed
neco3coffee <neco3coffee@gmail.com>
2021-10-24 00:59:55 +0900

    [Add] note/ create,update function  #18

--- lib/main.dart
+++ lib/main.dart
@@ -205,6 +205,58 @@ void main() async {
   ));
 }
 
+// CustomSnackBar--------------------------
+class CustomSnackBar {
+  static void showSnackBar({
+    required BuildContext? context,
+    required String title,
+    required String message,
+    required Color backgroundColor,
+  }) {
+    Get.snackbar(title, message,
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: backgroundColor,
+        titleText: Text(
+          title,
+          style: TextStyle(fontSize: 16),
+        ),
+        messageText: Text(
+          message,
+          style: TextStyle(fontSize: 16),
+        ),
+        colorText: Colors.white,
+        borderRadius: 8,
+        margin: EdgeInsets.all(16));
+  }
+}
+
+// -------------------------CustomSnackBar
+
+// CustomFullScreenDialog -------------------
+class CustomFullScreenDialog {
+  static void showDialog() {
+    Get.dialog(
+      WillPopScope(
+        child: Container(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+        onWillPop: () => Future.value(false),
+      ),
+      barrierDismissible: false,
+      barrierColor: Color(0xff141A31).withOpacity(.3),
+      useSafeArea: true,
+    );
+  }
+
+  static void cancelDialog() {
+    Get.back();
+  }
+}
+
+// -------------------CustomFullScreenDialog
+
 class LandingPageController extends GetxController {
   var tabIndex = 0.obs;
 
@@ -326,98 +378,16 @@ class LandingPage extends StatelessWidget {
   }
 }
 
-// // class Controller extends GetxController {
-// //   int count = 0;
-// //   void increment() {
-// //     count++;
-// //     // use update method to update all count variables
-// //     update();
-// //   }
-// // }
-
-var solidNotes = [
-  {
-    'title': 'title',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'shunta dayoooooo',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'third note dayooooooo',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'MVCモデル',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': '抽象化',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'OOP',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'キャメルケースとスネークケース',
-    'description': '_を使うのがスネークケースで最初の文字を大文字にするのがキャメルケーす',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'title',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'title',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'title',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'title',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-  {
-    'title': 'title',
-    'description': 'description',
-    'image':
-        'https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png',
-  },
-];
-
 class NoteModel {
+  String? docId;
   String? title;
   String? description;
   String? image;
 
-  NoteModel({this.title, this.description, this.image});
+  NoteModel({this.docId, this.title, this.description, this.image});
 
   NoteModel.fromMap(DocumentSnapshot data) {
+    docId = data.id;
     title = data["title"];
     description = data["description"];
     image = data["image"];
@@ -425,6 +395,9 @@ class NoteModel {
 }
 
 class NoteController extends GetxController {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  late TextEditingController nameController, addressController;
+
   FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
   late CollectionReference collectionReference;
   RxList<NoteModel> notes = RxList<NoteModel>([]);
@@ -432,12 +405,118 @@ class NoteController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+    nameController = TextEditingController();
+    addressController = TextEditingController();
     collectionReference = firebaseFirestore.collection("notes");
     notes.bindStream(getAllNotes());
   }
 
+  String? validateName(String value) {
+    if (value.isEmpty) {
+      return "Name can not be empty";
+    }
+    return null;
+  }
+
+  String? validateAddress(String value) {
+    if (value.isEmpty) {
+      return "Address can not be empty";
+    }
+    return null;
+  }
+
+  void saveUpdateEmployee(
+      String title, String description, String docId, int addEditFlag) {
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    formKey.currentState!.save();
+    if (addEditFlag == 1) {
+      CustomFullScreenDialog.showDialog();
+      collectionReference.add({
+        'title': title,
+        'description': description,
+        'image': 'http://beepeers.com/assets/images/commerces/default-image.jpg'
+      }).whenComplete(() {
+        CustomFullScreenDialog.cancelDialog();
+        clearEditingControllers();
+        Get.back();
+        CustomSnackBar.showSnackBar(
+            context: Get.context,
+            title: "Employee Added",
+            message: "Employee added successfully",
+            backgroundColor: Colors.green);
+      }).catchError((error) {
+        CustomFullScreenDialog.cancelDialog();
+        CustomSnackBar.showSnackBar(
+            context: Get.context,
+            title: "Error",
+            message: "Something went wrong",
+            backgroundColor: Colors.green);
+      });
+    } else if (addEditFlag == 2) {
+      //update
+      CustomFullScreenDialog.showDialog();
+      collectionReference.doc(docId).update(
+          {'title': title, 'description': description}).whenComplete(() {
+        CustomFullScreenDialog.cancelDialog();
+        clearEditingControllers();
+        Get.back();
+        CustomSnackBar.showSnackBar(
+            context: Get.context,
+            title: "Employee Updated",
+            message: "Employee updated successfully",
+            backgroundColor: Colors.green);
+      }).catchError((error) {
+        CustomFullScreenDialog.cancelDialog();
+        CustomSnackBar.showSnackBar(
+            context: Get.context,
+            title: "Error",
+            message: "Something went wrong",
+            backgroundColor: Colors.red);
+      });
+    }
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    nameController.dispose();
+    addressController.dispose();
+  }
+
   Stream<List<NoteModel>> getAllNotes() => collectionReference.snapshots().map(
       (query) => query.docs.map((item) => NoteModel.fromMap(item)).toList());
+
+  void clearEditingControllers() {
+    nameController.clear();
+    addressController.clear();
+  }
+
+  void deleteData(String docId) {
+    CustomFullScreenDialog.showDialog();
+    collectionReference.doc(docId).delete().whenComplete(() {
+      CustomFullScreenDialog.cancelDialog();
+      Get.back();
+      CustomSnackBar.showSnackBar(
+          context: Get.context,
+          title: "Employee Deleted",
+          message: "Employee deleted successfully",
+          backgroundColor: Colors.green);
+    }).catchError((error) {
+      CustomFullScreenDialog.cancelDialog();
+      CustomSnackBar.showSnackBar(
+          context: Get.context,
+          title: "Error",
+          message: "Something went wrong",
+          backgroundColor: Colors.red);
+    });
+  }
 }
 
 class NoteBinding extends Bindings {
@@ -453,26 +532,28 @@ class NotePage extends GetView<NoteController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print(controller.notes.length);
-          }, //newNote
-          child: Icon(Icons.add),
-        ),
-        appBar: AppBar(
-          title: Text('Note'),
-        ),
-        body: GridView.builder(
-            padding: EdgeInsets.all(10),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.618,
-                mainAxisSpacing: 16.8,
-                crossAxisSpacing: 10.0),
-            itemCount: controller.notes.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                  child: Container(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print(controller.notes.length);
+          _buildAddEditEmployeeView(text: 'ADD', addEditFlag: 1, docId: '');
+        }, //newNote
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Text('Note'),
+      ),
+      body: Obx(
+        () => GridView.builder(
+          padding: EdgeInsets.all(10),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.618,
+              mainAxisSpacing: 16.8,
+              crossAxisSpacing: 10.0),
+          itemCount: controller.notes.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: Container(
                 decoration: BoxDecoration(
                   image: DecorationImage(
                       image: NetworkImage("${controller.notes[index].image}"),
@@ -482,7 +563,16 @@ class NotePage extends GetView<NoteController> {
                       alignment: Alignment.bottomCenter),
                 ),
                 child: InkWell(
-                  onTap: () {},
+                  onTap: () {
+                    controller.nameController.text =
+                        controller.notes[index].title!;
+                    controller.addressController.text =
+                        controller.notes[index].description!;
+                    _buildAddEditEmployeeView(
+                        text: 'UPDATE',
+                        addEditFlag: 2,
+                        docId: controller.notes[index].docId!);
+                  },
                   child: Column(
                     children: [
                       // Text("${notes[index]['title']}"),
@@ -499,8 +589,111 @@ class NotePage extends GetView<NoteController> {
                     ],
                   ),
                 ),
-              ));
-            }));
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  _buildAddEditEmployeeView({String? text, int? addEditFlag, String? docId}) {
+    Get.bottomSheet(
+      Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(16),
+            topLeft: Radius.circular(16),
+          ),
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 16),
+          child: Form(
+            key: controller.formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${text} Note',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    controller: controller.nameController,
+                    validator: (value) {
+                      return controller.validateName(value!);
+                    },
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: 'Description',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    controller: controller.addressController,
+                    validator: (value) {
+                      return controller.validateAddress(value!);
+                    },
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints.tightFor(
+                        width: Get.context!.width, height: 45),
+                    child: ElevatedButton(
+                      child: Text(
+                        text!,
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                      onPressed: () {
+                        controller.saveUpdateEmployee(
+                            controller.nameController.text,
+                            controller.addressController.text,
+                            docId!,
+                            addEditFlag!);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  displayDeleteDialog(String docId) {
+    Get.defaultDialog(
+      title: "Delete Employee",
+      titleStyle: TextStyle(fontSize: 20),
+      middleText: 'Are you sure to delete employee ?',
+      textCancel: "Cancel",
+      textConfirm: "Confirm",
+      confirmTextColor: Colors.black,
+      onCancel: () {},
+      onConfirm: () {
+        controller.deleteData(docId);
+      },
+    );
   }
 }
 
